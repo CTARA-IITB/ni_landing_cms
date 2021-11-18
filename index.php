b7630659c31e4955d552029c27121df1b7104eac
Animesh Nautiyal <animesh.nauti@gmail.com>
2021-11-18 12:37:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if($path == 'www.nutritionindia.info'){
       http-equiv="Content-Security-Policy" 
       content="
         connect-src 'self'  https://dev.communitygis.net https://uat2.nutritionindia.info https://dev.nutritionindiainfo.in https://nutritionindia.info https://uat.nutritionindia.info https://www.google-analytics.com;
-        default-src 'none';
+        default-src 'self';
         img-src 'self' 'unsafe-inline' https://www.google-analytics.com data:;
         manifest-src 'self';
         script-src 'unsafe-inline' https://dev.communitygis.net https://uat2.nutritionindia.info https://dev.nutritionindiainfo.in https://nutritionindia.info https://www.google-analytics.com https://www.googletagmanager.com http://www.googletagmanager.com http://localhost:3000  https://uat.nutritionindia.info;
@@ -113,7 +113,7 @@ if($path == 'www.nutritionindia.info'){
 				<div class="yellow-round-big"></div>
 				<div class="yellow-round-small"></div>
 				<div class="div-carousel">
-					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="6000000">
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="6000">
 					  <div class="carousel-inner">
 						<div class="carousel-item active">
 						  <img class="d-block c-image" src="images/carousel/c1.png" alt="First slide">
