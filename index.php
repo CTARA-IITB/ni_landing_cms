8f31cf3aac88098788e641aa0ff35cde3647fd68
AnimeshN <nautiyalanimesh@gmail.com>
2024-12-14 15:44:55 +0530

    moved vh to responsive.css

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if($path == 'www.healthnutritionindia.in'){
 			</div>
 		</div>
 	</div>
-	<div class="p-3 col-12" style=" height:48vh">
+	<div class="p-3 col-12 half-screen">
 		<div class='text-center align-content-center for-mobile align-items-center'>
 			<!-- <div class="yellow-round-big"></div> -->
 			<div class="pt-4 col-md-12 col-lg-6 col-12 float-left for-mobile-1 display-none-desktop pb-ipad">
